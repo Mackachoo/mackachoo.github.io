7758fe9edaaac02579f2fb4a2c53806423246250
Dan <Mackachooo@gmail.com>
2023-05-29 02:40:44 +0100

    Update aboutme.dart
    Added CV profile

--- lib/content/aboutme.dart
+++ lib/content/aboutme.dart
@@ -3,5 +3,5 @@ import 'package:portfolio/service/sections.dart';
 
 Section aboutme = Section(
   title: "About Me",
-  body: const Placeholder(),
+  body: const Text("I am a driven and creative individual with a passion for Physics, Computer Science, andMathematics. I have a strong ability to learn and adapt quickly, as well as work independently oras part of a team. I am seeking a challenging position in the field of technology both hardwareand software where I can use my skills to make significant contribution."),
 );
