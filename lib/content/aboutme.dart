c21a0e044649e91a57c0213b873272916ddaee1c
Dan <Mackachooo@gmail.com>
2023-05-29 02:46:19 +0100

    Update aboutme.dart
    Fixed line breaks

--- lib/content/aboutme.dart
+++ lib/content/aboutme.dart
@@ -3,5 +3,6 @@ import 'package:portfolio/service/sections.dart';
 
 Section aboutme = Section(
   title: "About Me",
-  body: const Text("I am a driven and creative individual with a passion for Physics, Computer Science, andMathematics. I have a strong ability to learn and adapt quickly, as well as work independently oras part of a team. I am seeking a challenging position in the field of technology both hardwareand software where I can use my skills to make significant contribution."),
+  body: const Text(
+      "I am a driven and creative individual with a passion for Physics, Computer Science, and Mathematics. I have a strong ability to learn and adapt quickly, as well as work independently or as part of a team. I am seeking a challenging position in the field of technology both hardware and software where I can use my skills to make significant contribution."),
 );
