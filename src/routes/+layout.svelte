3b3b66a9e37c343159d00dec664a7a5a667fd702
Dan <Mackachooo@gmail.com>
2023-12-15 21:53:49 +0000

    Setup skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
     import "../app.css";
+    import Header from "../components/Header.svelte";
+    import Footer from "../components/Footer.svelte";
 </script>
 
-<slot />
+<div class="relative flex flex-col max-w-screen-xl mx-auto w-full min-h-screen">
+    <Header />
+    <slot />
+    <Footer />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      mirage: '#111b2a',
+      rust: '#8e4549',
+      tan: '#f39c54',
+      paradiso: '#347b83'
+    }
   },
   plugins: []
 };
