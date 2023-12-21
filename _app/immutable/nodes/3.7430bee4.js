5cd233043a40b366bcddd808fad80fd731572639
Dan <Mackachooo@gmail.com>
2023-12-21 23:11:58 +0000

    Updates

---
+++ _app/immutable/nodes/3.7430bee4.js
@@ -0,0 +1,12 @@
+import{s as f,n as r}from"../chunks/scheduler.a80508cc.js";import{S as v,i as b,g as u,s as g,h,z as m,c as y,k as p,a as l,f as c}from"../chunks/index.20ee9d5b.js";const w=({params:s})=>({education:[],qualifications:[]}),P=Object.freeze(Object.defineProperty({__proto__:null,load:w},Symbol.toStringTag,{value:"Module"})),_=""+new URL("../assets/About.9bc2c1f7.jpg",import.meta.url).href;function I(s){let e,i=`<div><h2 class="border-tan border-b-4 mb-5">A Bit About Me</h2> <p>I hope you are doing well? I am extremely interested in this role
+            and I have a solid background in Computer Science and Physics that I
+            believe will make me perfect for it. I have successfully built
+            websites using Python and Django with React and Svelte as the front
+            end. I&#39;ve also used Python heavily for data analysis, physics
+            simulations and other personal projects. I have experience with
+            Flutter, Java, C and MIPS and bring a strong understanding of
+            hardware and networking.</p> <br/> <p>Over the years I&#39;ve been in all levels of project management and
+            collaboration, which make me well-prepared for working with
+            cross-functional teams and contributing to in dynamic environments.
+            I hold a Master&#39;s degree in Computational Physics and I am genuinely
+            passionate about technology and new ways of using it help people.</p></div> <img src="${_}" alt="Graduation" class="image mx-10 mt-2 h-[60vh]"/>`,n,t,d="Education and Qualifications";return{c(){e=u("div"),e.innerHTML=i,n=g(),t=u("h2"),t.textContent=d,this.h()},l(a){e=h(a,"DIV",{class:!0,"data-svelte-h":!0}),m(e)!=="svelte-1yu7ykq"&&(e.innerHTML=i),n=y(a),t=h(a,"H2",{class:!0,"data-svelte-h":!0}),m(t)!=="svelte-n91u73"&&(t.textContent=d),this.h()},h(){p(e,"class","flex my-5"),p(t,"class","border-tan border-b-4 mb-5")},m(a,o){l(a,e,o),l(a,n,o),l(a,t,o)},p:r,i:r,o:r,d(a){a&&(c(e),c(n),c(t))}}}function x(s,e,i){let{data:n}=e;return s.$$set=t=>{"data"in t&&i(0,n=t.data)},[n]}class S extends v{constructor(e){super(),b(this,e,x,I,f,{data:0})}}export{S as component,P as universal};
