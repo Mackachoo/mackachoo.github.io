a881a5b8b956b0e7360f47fccf972ad97a055933
Dan <Mackachooo@gmail.com>
2023-12-23 23:59:32 +0000

    Added contact dialog
    Used google forms, should make custom form in future

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,8 +1,7 @@
-<script>
-    /**
-     * @type {number}
-     */
-    let y;
+<script script="ts">
+    // @ts-nocheck
+
+    let dialog;
 </script>
 
 <div
@@ -14,7 +13,9 @@
             data-tooltip-target="tooltip-dark"
             data-tooltip-placement="bottom"
             type="button"
-            class=""
+            on:click={() => {
+                dialog.showModal();
+            }}
         >
             <i class="fa-solid fa-sharp fa-terminal text-shuttle m-2" />
         </button>
@@ -26,4 +27,37 @@
     </div>
 </div>
 
-<svelte:window bind:scrollY={y} />
+<dialog bind:this={dialog}>
+    <div
+        class="border-4 border-paradiso bg-mirage text-white flex flex-col justify-between items-center p-5"
+    >
+        <div class="flex">
+            <a
+                class="button text-rust border-rust m-3"
+                href="mailto:dan@dtbmail.com">Dan@dtbmail.com</a
+            >
+            <a class="button text-rust border-rust m-3" href="tel:+447738612600"
+                >+44 778612600</a
+            >
+        </div>
+
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSePEvpWma6fBbSMWnx7fMzyoIzy4e3hg4TUJZ9UF8xIORpG1A/viewform?embedded=true"
+            width="640"
+            height="540"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+        >
+            Loading…
+        </iframe>
+
+        <button
+            class="button w-32 mt-5"
+            on:click={() => {
+                dialog.close();
+            }}>Close</button
+        >
+    </div>
+</dialog>
