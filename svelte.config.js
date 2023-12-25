70c2dfe22302b9252184fe9000ba8f9c04b26660
Dan <Mackachooo@gmail.com>
2023-12-25 00:39:37 +0000

    Clean up

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,6 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		// paths: {
-		// 	base: process.env.NODE_ENV === 'production' ? '/mackachoo.github.io' : '',
-		// }
 	}
 
 };
