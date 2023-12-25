55ced9c394d2e7d1a3f882b4cbc8607d1a687ac3
Dan <Mackachooo@gmail.com>
2023-12-25 00:35:20 +0000

    Unpacked old flutter project

--- .firebase/portfolio-5ab98/functions/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.aec73831.js","_app/immutable/chunks/scheduler.cc9b4bb6.js","_app/immutable/chunks/index.ee4d2bbe.js","_app/immutable/chunks/singletons.9c99912a.js"];
-export const stylesheets = [];
-export const fonts = [];
--- old-version/README.md
+++
@@ -1,7 +0,0 @@
-# Portfolio
-
-A Portfolio website to showcase my projects, experience and education and to allow people to contact me.
-
-## Getting Started
-
-Hosted at mackachoo.github.io and dtbmail.com.
