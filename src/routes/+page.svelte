1df975a9f48ea239fe04f8f739e4c2eab190ceed
Dan <Mackachooo@gmail.com>
2025-12-15 02:32:20 +0000

    Update job title and affiliation in page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
             I'm <span class="text-tan">Dan Buxton</span>
         </h1>
         <h2 class="my-4 text-moon">
-            A Software Engineer currently working at <b>Morgan Stanley</b> with
+            A Software Engineer and the director of <b>Skiwi</b> with
             a
             <b>Masters in Computational Physics</b> from the University of Edinburgh
         </h2>
